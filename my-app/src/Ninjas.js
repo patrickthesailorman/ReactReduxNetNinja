b16fca92fd5adba483581c8349c9df483b9bbebb
Patrick McCarty <patrickthesailorman@gmail.com>
2018-08-26 11:33:19 -0400

    Add ternary

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 
 const Ninjas = ({ninjas}) => {
+    // const ninjaList = ninjas.map(ninja => {
+    //     if (ninja.age > 20){
+    //         return(
+    //             <div className="ninja" key ={ninja.id}>
+    //             <div>Name: { ninja.name }</div>
+    //             <div> Age: { ninja.age }</div>
+    //             <div>Belt: { ninja.belt }</div>
+    //         </div>
+    //         )
+    //     } else {
+    //         return null
+    //     }
+    // })
     const ninjaList = ninjas.map(ninja => {
-        if (ninja.age > 20){
-            return(
-                <div className="ninja" key ={ninja.id}>
+        return ninja.age > 20 ? (
+            <div className="ninja" key ={ninja.id}>
                 <div>Name: { ninja.name }</div>
                 <div> Age: { ninja.age }</div>
                 <div>Belt: { ninja.belt }</div>
-            </div>
-            )
-        } else {
-            return null
-        }
-    })
+           </div>
+        ) : null
+    });
     return(
         <div className="ninja-List">
             { ninjaList }
