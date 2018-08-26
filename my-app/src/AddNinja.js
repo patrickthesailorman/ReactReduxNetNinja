cfd7acb116d4f48193cac955b3e2dd8cc3ca18b5
Patrick McCarty <patrickthesailorman@gmail.com>
2018-08-26 15:46:59 -0400

    Add handleChange

--- my-app/src/AddNinja.js
+++ my-app/src/AddNinja.js
@@ -6,16 +6,21 @@ class AddNinja extends Componenet {
         age: null,
         belt: null
     }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
     render(){
         return (
             <div>
                 <form>
                     <label htmlFor="name">Name: </label>
-                    <input type="text" id="name" onChange={} />
+                    <input type="text" id="name" onChange={this.handleChange} />
                     <label htmlFor="age">Age: </label>
-                    <input type="text" id="age" onChange={} />
+                    <input type="text" id="age" onChange={this.handleChange} />
                     <label htmlFor="belt">Belt: </label>
-                    <input type="text" id="belt" onChange={} />
+                    <input type="text" id="belt" onChange={this.handleChange} />
                     <button>Submit</button>
                 </form>
             </div>
