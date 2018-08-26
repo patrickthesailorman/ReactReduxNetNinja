4215215d0ec9c66622485ce0903231769df00dfd
Patrick McCarty <patrickthesailorman@gmail.com>
2018-08-26 12:27:50 -0400

    Add ninjaList

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -14,18 +14,20 @@ const Ninjas = ({ninjas}) => {
     //         return null
     //     }
     // })
-    const ninjaList = ninjas.map(ninja => {
-        return ninja.age > 20 ? (
-            <div className="ninja" key ={ninja.id}>
-                <div>Name: { ninja.name }</div>
-                <div> Age: { ninja.age }</div>
-                <div>Belt: { ninja.belt }</div>
-           </div>
-        ) : null
-    });
+
     return(
         <div className="ninja-List">
-            { ninjaList }
+            { 
+                ninjas.map(ninja => {
+                    return ninja.age > 20 ? (
+                        <div className="ninja" key ={ninja.id}>
+                            <div>Name: { ninja.name }</div>
+                            <div> Age: { ninja.age }</div>
+                            <div>Belt: { ninja.belt }</div>
+                        </div>
+                    ) : null
+                 })
+            }
         </div>
     )
 }
