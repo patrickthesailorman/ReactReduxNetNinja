9f68f49bae67f3cf6823a916a513d7a1b9becd8f
Patrick McCarty <patrickthesailorman@gmail.com>
2018-08-26 11:04:04 -0400

    Add ninjas

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
-const Ninjas = () => {
-    const { ninjas } = this.props;
+const Ninjas = ({ninjas}) => {
     const ninjaList = ninjas.map(ninja => {
         return(
             <div className="ninja" key ={ninja.id}>
