7ac35b55fe4c5fa851c19c53964302170d86e2e4
Patrick McCarty <patrickthesailorman@gmail.com>
2018-08-26 12:39:20 -0400

    Add addNinja.js

---
+++ my-app/src/AddNinja.js
@@ -0,0 +1,13 @@
+import React, { Componenet } from 'react'
+
+class AddNinja extends Componenet {
+    render(){
+        return (
+            <div>
+                <form>
+                    <label htmlFor="name">Name: </label>
+                </form>
+            </div>
+        )
+    }
+}
