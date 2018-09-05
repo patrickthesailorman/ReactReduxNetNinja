82e074815738f1befbdca58e69225abc73153cef
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-05 16:59:01 -0400

    Add deleteTodo

--- react-redux-complete-playlist/todoapp/src/Todos.js
+++ react-redux-complete-playlist/todoapp/src/Todos.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Todos = ({todos}) => {
+const Todos = ({todos, deleteTodo}) => {
     const todoList = todos.length ? (
         todos.map(todo => {
             return (
