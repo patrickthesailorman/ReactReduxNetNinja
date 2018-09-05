4af8c2a8d558e83421f155eb24e608e69b747453
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-04 20:07:49 -0400

    Add state

--- react-redux-complete-playlist/todoapp/src/App.js
+++ react-redux-complete-playlist/todoapp/src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'buy some eggs'}
+    ]
+  }
   render() {
     return (
       <div className="App">
