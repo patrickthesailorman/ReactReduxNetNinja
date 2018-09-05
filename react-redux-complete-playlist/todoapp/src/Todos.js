dfb744800665d19a5854afdad0b842b724c8a0a5
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-04 20:35:19 -0400

    Add todoList

--- react-redux-complete-playlist/todoapp/src/Todos.js
+++ react-redux-complete-playlist/todoapp/src/Todos.js
@@ -4,7 +4,7 @@ const Todos = ({todos}) => {
     const todoList = todos.length ? (
         todos.map(todo => {
             return (
-                <div className="collection-item">
+                <div className="collection-item" key={todo.id}>
                     <span>{todo.content}</span>
                 </div>
             )
@@ -14,7 +14,9 @@ const Todos = ({todos}) => {
     )
     return (
         <div className="todos collection">
-
+            {todoList}
         </div>
     )
 }
+
+export default Todos;
