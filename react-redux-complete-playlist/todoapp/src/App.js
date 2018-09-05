6321f706349f379993f0581ae16782511d93a49b
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-05 16:57:59 -0400

    Add this.deleteTodo

--- react-redux-complete-playlist/todoapp/src/App.js
+++ react-redux-complete-playlist/todoapp/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's:</h1>
-        <Todos todos={this.state.todos} />       
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />       
       </div>
     );
   }
