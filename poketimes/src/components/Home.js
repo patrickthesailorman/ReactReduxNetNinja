34a575a809ed8192502068385fa59299ce4f61e0
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-06 18:55:34 -0400

    Add Lorem Ipsum

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -4,6 +4,9 @@ const Home = () => {
     return (
         <div className="container">
             <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi eligendi natus rem asperiores explicabo omnis voluptate fugiat eveniet sunt culpa, totam illum tempore nulla harum enim optio libero perferendis! Cupiditate.</p>
         </div>
     )
 }
+
+export default Home
