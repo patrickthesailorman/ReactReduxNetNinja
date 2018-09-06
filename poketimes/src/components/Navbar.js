42818f25a1dbbfa23aff520d0e21eb62e92531a6
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-06 19:47:18 -0400

    Add Navbar routes

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -5,6 +5,11 @@ const Navbar = () => {
         <nav className="nav-wrapper red darken-3">
             <div className="container">
                 <a className="brand-logo">Poke'Times</a>
+                <ul className="right">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
             </div>
         </nav>
     )
