04b4e96e0bdfbec60d84cfde64e482a718f64cca
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-06 19:19:47 -0400

    Add Navbar html

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        
+        <nav className="nav-wrapper red darken-3">
+            <div className="container">
+                <a className="brand-logo">Poke'Times</a>
+            </div>
+        </nav>
     )
 }
