9ced495757e0f0f205c214b4aa5154a6dfb0edea
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-06 20:18:02 -0400

    Add BrowserRouter

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
+import { BrowserRouter } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
-        
+        <Navbar />
       </div>
+      </BrowserRouter>
     );
   }
 }
