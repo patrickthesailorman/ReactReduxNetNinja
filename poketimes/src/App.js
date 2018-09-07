85032f995343fb8f26e200e37da80a521329f002
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-06 20:40:38 -0400

    Add import Home

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
 import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './components/Home'
 
 class App extends Component {
   render() {
