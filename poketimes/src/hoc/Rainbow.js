53cd3b781fd126b7fce6ad1835b71c2552bf98c0
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-07 22:08:40 -0400

    Add className

--- poketimes/src/hoc/Rainbow.js
+++ poketimes/src/hoc/Rainbow.js
@@ -4,4 +4,5 @@ const Rainbow = (WrappedComponent) => {
 
     const colours = ['red', 'pink', 'orange', 'blue', 'green', 'yellow']
     const randomColour = colours[Math.floor(Math.random() *5)];
+    const className = randomColour += '-text'
 }
