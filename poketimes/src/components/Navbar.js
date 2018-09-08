306ce4aa45462a860c32d3def39b727df0c51396
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-07 21:44:29 -0400

    console props

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = (props) => {
-    setTimeout(() => {
-        props.history.push('/about')
-    }, 2000);
+    console.log(props)
+    // setTimeout(() => {
+    //     props.history.push('/about')
+    // }, 2000);
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
