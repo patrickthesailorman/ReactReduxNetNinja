e8d24f53278e629aae6afc51064db1911bb514c7
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-07 22:07:26 -0400

    Add randomColours

--- poketimes/src/hoc/Rainbow.js
+++ poketimes/src/hoc/Rainbow.js
@@ -3,5 +3,5 @@ import React from 'react'
 const Rainbow = (WrappedComponent) => {
 
     const colours = ['red', 'pink', 'orange', 'blue', 'green', 'yellow']
-
+    const randomColour = colours[Math.floor(Math.random() *5)];
 }
