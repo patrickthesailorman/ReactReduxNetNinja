0980effad9261e27a99f46fc2a7e721bc6e6c255
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-08 21:44:44 -0400

    Component

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Home = () => {
-    return (
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi eligendi natus rem asperiores explicabo omnis voluptate fugiat eveniet sunt culpa, totam illum tempore nulla harum enim optio libero perferendis! Cupiditate.</p>
-        </div>
-    )
+class Home extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h4 className="center">Home</h4>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi eligendi natus rem asperiores explicabo omnis voluptate fugiat eveniet sunt culpa, totam illum tempore nulla harum enim optio libero perferendis! Cupiditate.</p>
+            </div>
+        )
+    }
 }
 
 export default Home
