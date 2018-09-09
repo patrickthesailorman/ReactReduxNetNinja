73d0a403cbd846fb398ea90914220fdf59538bb1
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-08 21:47:08 -0400

    Add jsonplaeholder

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 class Home extends Component {
     componentDidMount(){
-        
+        axios.get('https://jsonplaceholder.typicode.com/posts')
     }
     render() {
         return (
