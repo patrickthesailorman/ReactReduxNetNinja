404cad39a0eab745232a231200af1bc6afc49558
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-08 22:29:56 -0400

    Add posts

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import axios from 'axios'
 
 class Home extends Component {
+    state = {
+        posts: [ ]
+    }
     componentDidMount(){
         axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(res => {
