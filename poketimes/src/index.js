f69e94445d9f09238cfd8bebfe0883c24482dc50
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-11 17:58:20 -0400

    import provider

--- poketimes/src/index.js
+++ poketimes/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 const store = createStore();
 
