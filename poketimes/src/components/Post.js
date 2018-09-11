e2056e996752dd780d7a0eb31692e5edc9f55b25
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-11 19:29:53 -0400

    Add connect

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import { connect } from 'react-redux'
+// import axios from 'axios'
 
 class Post extends Component {
-    state = {
-        post: null
-    }
-    componentDidMount(){
-        let id = this.props.match.params.post_id;
-        axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
-            .then(res => {
-                this.setState({
-                    post: res.data
-                })
-            })
-      }
+    // state = {
+    //     post: null
+    // }
+    // componentDidMount(){
+    //     let id = this.props.match.params.post_id;
+    //     axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+    //         .then(res => {
+    //             this.setState({
+    //                 post: res.data
+    //             })
+    //         })
+    //   }
     render() {
 
         const post = this.state.post ? (
